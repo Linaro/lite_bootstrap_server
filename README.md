ac8b0be00a1d044e8a6ed4f6d714b53dd3c62ade
Kevin Townsend <kevin.townsend@linaro.org>
2020-06-24 18:52:20 +0200

    httpserver: Enable TLS support
    This commit enables TLS for the HTTP server via a self-signed
    certificate.
    
    Signed-off-by: Kevin Townsend <kevin.townsend@linaro.org>

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ A basic, proof-of-concept certificate authority (CA) and utility written in Go.
 This utility can be used to validate and sign certificate signing requests (CSRs),
 list previously registered devices, and enables a basic HTTP server that can be
 used to communicate with the CA and it's underlying device registry.
+
+## Key Generation for HTTP Server
+
+The HTTP server requires a private key for TLS, which can be generated via:
+
+```bash
+$ openssl ecparam -name secp256r1 -genkey -out SERVER.key
+```
+
+You can then generate a self-signed X.509 public key via:
+
+```bash
+$ openssl req -new -x509 -sha256 -days 3650 -key SERVER.key -out SERVER.crt \
+        -subj "/O=Linaro/CN=CAWebServer"
+```
--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -2,15 +2,33 @@ package httpserver
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func Start() {
+	// Make sure the server key and certificate exist
+	if !fileExists("SERVER.key") || !fileExists("SERVER.crt") {
+		log.Fatal("Server certificate and key not found. See README.md.")
+	}
+	fmt.Println("Starting HTTPS server on port https://localhost:443")
 	http.HandleFunc("/", HelloServer)
-	fmt.Println("Starting HTTPS server on port 8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServeTLS(":443", "SERVER.crt", "SERVER.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServeTLS: ", err)
+	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "success!")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("CA HTTP server.\n"))
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
