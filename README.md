a5c3e50a52caf4acf8235ef5318895cdd9591a14
Kevin Townsend <kevin.townsend@linaro.org>
2020-07-01 17:52:18 +0200

    httpserver: Add REST API endpoint draft
    This commit adds a first draft of a REST API based on RFC4210 (CMP).
    
    Signed-off-by: Kevin Townsend <kevin.townsend@linaro.org>

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This utility can be used to validate and sign certificate signing requests (CSRs
 list previously registered devices, and enables a basic HTTP server that can be
 used to communicate with the CA and it's underlying device registry.
 
-## Key Generation for HTTP Server
+## Quick Setup 
+
+### Key Generation for HTTP Server
 
 The HTTP server requires a private key for TLS, which can be generated via:
 
@@ -21,34 +23,59 @@ $ openssl req -new -x509 -sha256 -days 3650 -key SERVER.key -out SERVER.crt \
         -subj "/O=Linaro, LTD/CN=LinaroCA HTTP Server Cert"
 ```
 
+This public key should be available on any devices connecting to the CA to
+verify that we are communicating with the intended CA server.
+
 The contents of the certificate can be verified via:
 
 ```bash
 $ openssl x509 -in SERVER.crt -noout -text
-Certificate:
-    Data:
-        Version: 1 (0x0)
-        Serial Number: 9242433173333109773 (0x8043b800acce6c0d)
-    Signature Algorithm: ecdsa-with-SHA256
-        Issuer: O=Linaro, LTD, CN=LinaroCA HTTP Server Cert
-        Validity
-            Not Before: Jun 25 11:58:29 2020 GMT
-            Not After : Jun 23 11:58:29 2030 GMT
-        Subject: O=Linaro, LTD, CN=LinaroCA HTTP Server Cert
-        Subject Public Key Info:
-            Public Key Algorithm: id-ecPublicKey
-                Public-Key: (256 bit)
-                pub: 
-                    04:8b:9e:75:7b:92:1a:4c:c9:86:73:f0:4d:a1:04:
-                    41:17:81:8e:79:a5:5a:ce:df:b9:81:28:a4:43:49:
-                    20:f2:e6:af:54:77:dc:44:23:4f:d2:80:96:a3:aa:
-                    b1:c5:d4:7f:be:cb:1a:d1:d1:b7:a5:4b:d7:8d:19:
-                    28:75:19:0d:e4
-                ASN1 OID: prime256v1
-                NIST CURVE: P-256
-    Signature Algorithm: ecdsa-with-SHA256
-         30:45:02:20:14:e4:dc:44:68:9b:47:4b:ef:3c:fe:f6:95:bd:
-         22:d8:89:ab:da:74:67:89:b3:b3:b1:52:91:55:98:fc:ca:76:
-         02:21:00:d0:98:eb:5d:b7:c1:62:fe:80:e8:fa:bd:64:f5:d7:
-         51:28:a2:18:42:8f:ec:f9:97:fd:18:9e:e1:71:9a:b4:ac
 ```
+
+## HTTPS Server
+
+To initialise the HTTPS server on the default port (443), run:
+
+> :information_source: Use the `-p <port>` flag to change the port number.
+
+```bash 
+$ linaroca server start
+Starting HTTPS server on port https://localhost:443
+```
+
+This will serve web pages from root, and handle REST API requests from the
+`/api/v1` sub-path.
+
+### REST API Endpoints
+
+API based loosely on [CMP (RFC4210)](https://tools.ietf.org/html/rfc4210).
+
+#### Initialisation Request: `/api/v1/ir` **GET**
+
+This endpoint is used to register new (previously unregistered) devices into
+the management system. Initialisation must occur before certificates can be
+requested.
+
+#### Certification Request: `/api/v1/cr` **POST**
+
+This endpoint is used for certificate requests from existing devices who
+wish to obtain new certificates.
+
+#### Certification Request from PKCS10: `/api/v1/p10cr` **POST**
+
+This endpoint is used for certificate requests from existing devices who
+wish to obtain new certificates, providing a PKCS#10 CSR file for the request.
+
+#### Certificate Status Request: `api/v1/cs` **GET**
+
+Requests the status of the specified certificate.
+
+#### Key Update Request: `api/v1/kur` **POST**
+
+Request an update to an existing (non-revoked and non-expired) certificate. An
+update is a replacement certificate containing either a new subject public
+key or the current subject public key.
+
+#### Key Revocation Request: `api/v1/krr` **POST**
+
+Requests the revocation of an existing certificate registration.
--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -10,36 +10,56 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
+// Initialisation request handler
+func irGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
+	w.Write([]byte(`{"message": "ir GET called"}`))
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "post called"}`))
-}
-
-func put(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(`{"message": "put called"}`))
-}
-
-func delete(w http.ResponseWriter, r *http.Request) {
+// Certification request handler
+func crPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "delete called"}`))
+	w.Write([]byte(`{"message": "cr POST called"}`))
 }
 
+// Certification request from PKCS#10 handler
+func p10crPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "p10cr POST called"}`))
+}
+
+// Certificate status request handler
+func csGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "cs GET called"}`))
+}
+
+// Key update request handler
+func kurPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "kur POST called"}`))
+}
+
+// Key revocation request
+func krrPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "krr POST called"}`))
+}
+
+// RESET API catch all handler
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"message": "not found"}`))
+	w.Write([]byte(`{"message": "endpoint not found"}`))
 }
 
+// Test endpoint: https://localhost/api/v1/status/123?format=cbor
 func statusGet(w http.ResponseWriter, r *http.Request) {
 	pathParams := mux.Vars(r)
 	w.Header().Set("Content-Type", "application/json")
@@ -64,18 +84,30 @@ func statusGet(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Root page handler
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Linaro CA HTTP server\n"))
+}
+
 // Start the HTTP Server
 func Start(port int16) {
 	r := mux.NewRouter()
+
+	// Setup the REST API subrouter
 	api := r.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("", get).Methods(http.MethodGet)
-	api.HandleFunc("", post).Methods(http.MethodGet)
-	api.HandleFunc("", put).Methods(http.MethodGet)
-	api.HandleFunc("", delete).Methods(http.MethodGet)
+	api.HandleFunc("/ir", irGet).Methods(http.MethodGet)
+	api.HandleFunc("/cr", crPost).Methods(http.MethodPost)
+	api.HandleFunc("/p10cr", p10crPost).Methods(http.MethodPost)
+	api.HandleFunc("/cs", csGet).Methods(http.MethodGet)
+	api.HandleFunc("/kur", kurPost).Methods(http.MethodPost)
+	api.HandleFunc("/krr", krrPost).Methods(http.MethodPost)
+	api.HandleFunc("/status/{deviceID}", statusGet).Methods(http.MethodGet)
 	api.HandleFunc("", notFound)
 
-	// Can be tested with a GET call to https://localhost/api/v1/status/123?format=cbor
-	api.HandleFunc("/status/{deviceID}", statusGet).Methods(http.MethodGet)
+	// Handle standard requests. Routes are tested in the order they are added,
+	// so these will only be handled if they don't match anything above.
+	r.HandleFunc("/", home)
 
 	// Make sure the server key and certificate exist
 	if !fileExists("SERVER.key") || !fileExists("SERVER.crt") {
@@ -87,11 +119,6 @@ func Start(port int16) {
 	if err != nil {
 		log.Fatal("ListenAndServeTLS: ", err)
 	}
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Linaro CA HTTP server.\n"))
 }
 
 func fileExists(filename string) bool {
