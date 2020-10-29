3a85aac9b67d7987cb8cd76ec70790e71d6f63df
David Brown <david.brown@linaro.org>
2020-10-29 14:34:19 -0600

    Split out client parts into run-client.sh
    It should now be possible to run:
    
        ./run-server.sh
    
    in one terminal window, and once the HTTPS server has started, run
    
        ./run-client.sh
    
    in another window.  It is possible to run the client multiple times,
    although the script will overwrite the USER.* files on each subsequent
    run, losing the particulars of a given device key.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- run-server.sh
+++ run-server.sh
@@ -22,30 +22,5 @@ openssl req -new -x509 -sha256 -days 3650 -key SERVER.key -out SERVER.crt \
 # Generate the CA certificate
 ./linaroca cakey generate
 
-# Generate private user key
-openssl ecparam -name prime256v1 -genkey -out USER.key
-
-# Generate a user CSR with a random UUID
-openssl req -new -key USER.key -out USER.csr \
-    -subj "/O=$HOSTNAME/CN=$(uuidgen | tr '[:upper:]' '[:lower:]')"
-
-# Convert CSR to JSON
-go run make_csr_json.go
-
-echo "HTTP cert, CA cert and USER CSR generated."
-
 # Start linaroca
 ./linaroca server start -p 1443
-
-# At this point we can submit the CSR via:
-# $ wget --ca-certificate=SERVER.crt \
-#     --post-file USER.json \
-#     https://localhost:1443/api/v1/cr \
-#     -O USER.cr
-#
-# Convert it to DER then PEM:
-# $ jq -r '.Cert' < USER.cr | base64 --decode > USER.der
-# $ openssl x509 -in USER.der -inform DER -out USER.crt -outform PEM
-#
-# Then, verify it:
-# $ openssl x509 -in USER.crt -noout -text
