93b51e320db1b5e5831294efe7ccdcd89ae0faf8
David Brown <david.brown@linaro.org>
2022-02-15 13:34:45 -0700

    Split invocation scripts to allow separate invocation.
    First, create a `setup-ca.sh` script that is intended to be run once per
    developer.  This will create the CA and SERVER key and certificates.
    The script will exit with an error if either of these is present, and it
    will have to be cleaned up manually.
    
    Second, the `run-server.sh` script just invokes the linaroca server.  It
    can be run multiple times, and will not create new keys each time.
    
    Lastly, `run-client.sh` is changed to `new-device.sh`.  The keys are
    placed in files named after the device ID, so that multiple devices can
    be registered and experimented with.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- run-server.sh
+++ run-server.sh
@@ -2,25 +2,13 @@
 
 HOSTNAME=localhost
 
+if [ ! -f CA.crt -o ! -f CA.key -o ! CA.crt ]; then
+	echo "Server certificates not present.  Please run ./setup-ca.sh"
+	exit 1
+fi
+
 # Build linaroca
 go build || exit 1
-
-# Clean up previous artifacts
-rm CA.* SERVER.* USER.*
-
-# Clean up the database (this is for testing), since once we've
-# removed the CA.crt, those certs are fairly meanmingless.
-rm CADB.db
-
-# Generate private key for the HTTP server
-openssl ecparam -name secp256r1 -genkey -out SERVER.key
-
-# Generate a self-signed certificate
-openssl req -new -x509 -sha256 -days 3650 -key SERVER.key -out SERVER.crt \
-        -subj "/O=Linaro, LTD/CN=$HOSTNAME"
-
-# Generate the CA certificate
-./linaroca cakey generate
 
 # Start linaroca
 ./linaroca server start -p 1443
