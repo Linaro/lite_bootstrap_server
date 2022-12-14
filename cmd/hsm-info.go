590d3d6197a97e274aba4196b7e00eb3bcea8272
David Brown <david.brown@linaro.org>
2022-12-14 15:29:24 -0700

    Clean up the key generation comments
    To make it easier to work with creating keys, show commands for deleting
    the keys from the sample `hsm info` command.  In addition, demonstrate
    that the FindObjectsByUUID` call does work.

--- cmd/hsm-info.go
+++ cmd/hsm-info.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/Linaro/lite_bootstrap_server/hsm"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
 
@@ -39,21 +40,36 @@ var hsmInfoCmd = &cobra.Command{
 
 		// Generate test keys with:
 		//
+		// $ #ECUUID=$(uuidgen | tr -d -)
+		// $ #RSAUUID=$(uuidgen | tr -d -)
+		// $ ECUUID=90adf2460d194726ac2235071c5c148b
+		// $ RSAUUID=8ce44ddceced463bb6e191efcbb25edb
 		// $ pkcs11-tool --module /opt/homebrew/Cellar/softhsm/2.6.1/lib/softhsm/libsofthsm2.so \
-		//   -l -p 1234 -k --id `uuidgen | tr -d -` --label "Test EC Key" \
+		//   -l -p 1234 -k --id $ECUUID --label "Test EC Key" \
 		//   --key-type EC:prime256v1
 		//
 		// $ pkcs11-tool --module /opt/homebrew/Cellar/softhsm/2.6.1/lib/softhsm/libsofthsm2.so \
-		//   -l -p 1234 -k --id `uuidgen | tr -d -` --label "Test RSA Key" \
+		//   -l -p 1234 -k --id $RSAUUID --label "Test RSA Key" \
 		//   --key-type rsa:2048
+		//
+		// The keys can be deleted with:
+		//
+		// $ pkcs11-tool --module <...> \
+		//   -l -p 1234 -b --type privkey --label "Test EC Key"
+		//
+		// $ pkcs11-tool --module <...> \
+		//   -l -p 1234 -b --type pubkey --label "Test EC Key"
+		//
+		// etc for the RSA key
 
 		// Search for the Root CA Key
 		// uuid, _ := uuid.Parse("8ce44ddc-eced-463b-b6e1-91efcbb25edb") // RSA
-		// uuid, _ := uuid.Parse("90adf246-0d19-4726-ac22-35071c5c148b") // EC
-		// if err := hsm.FindObjectsByUUID(uuid, 10); err != nil {
-		// 	fmt.Printf("Unhandled PKCS#11 error: %s\n", err)
-		// 	os.Exit(1)
-		// }
+		uuid, _ := uuid.Parse("90adf246-0d19-4726-ac22-35071c5c148b") // EC
+		if err := hsm.FindObjectsByUUID(uuid, 10); err != nil {
+			fmt.Printf("Unhandled PKCS#11 error: %s\n", err)
+			os.Exit(1)
+		}
+
 		if err := hsm.FindObjectsByLabel("Test EC Key", 10); err != nil {
 			fmt.Printf("Unhandled PKCS#11 error: %s\n", err)
 			os.Exit(1)
